f001f1cec1b56041e0ce885c0ec6c802cfe0ab71
seong hyeon <sonhyon86@gmail.com>
2025-10-30 02:12:36 +0900

    Update 02_TFI.R

--- 02_TFI.R
+++ 02_TFI.R
@@ -1,8 +1,28 @@
+library(dplyr)
+
 load("./tourist_density.rdata")
+load("./busstop_sum.rdata")
 load("./emission_region.rdata")
+
 head(tourist_density) #관광 과밀도
+head(busstop_sum) #생활 불편도
 head(emission_region) #환경부화도
 
+# 1. 컬럼 이름 맞추기
+emission_region <- emission_region %>%
+  rename(지역명 = 읍면동명)
+
+# 2. 두 데이터 합치기 (지역 기준)
+merged_data <- tourist_density %>%
+  left_join(emission_region, by = "지역명")
+
+# 결과 확인
+head(merged_data)
 
 
-#TFI <- 0.4*tourist_density + 0.3* + 0.3*emission_region 
+merged_data <- merged_data %>%
+  mutate(TFI = 0.4 * 관광객밀도 + 0.3 * 총배출량)
+
+head(merged_data)
+
+save(merged_data, file = "./merged_data.rdata")
